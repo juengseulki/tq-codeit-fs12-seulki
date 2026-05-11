57b87b6e83f41d5070343e41df10d8e4d71e5f14
jeungseulki <jsk432726@gmail.com>
2026-05-11 16:09:44 +0900

    실습 완료

--- src/api/todos.js
+++ src/api/todos.js
@@ -2,12 +2,26 @@
 const API_URL = "http://localhost:4000/todos";
 
 // 할 일 목록 조회
-export const fetchTodos = async () => {
-  const response = await fetch(API_URL);
+export const fetchTodos = async (options = {}) => {
+  const page = options.page || 1;
+  console.log("fetchTodos 호출, 페이지:", page);
+
+  const limit = 5;
+  const response = await fetch(`${API_URL}?_page=${page}&_limit=${limit}`);
+
   if (!response.ok) {
     throw new Error("서버에서 데이터를 가져오는데 실패했습니다.");
   }
-  return await response.json();
+
+  const totalCount = response.headers.get("X-Total-Count");
+  const data = await response.json();
+
+  return {
+    todos: data,
+    totalCount: parseInt(totalCount || "0"),
+    totalPages: Math.ceil(parseInt(totalCount || "0") / limit),
+    currentPage: page,
+  };
 };
 
 // 할 일 상세 조회
